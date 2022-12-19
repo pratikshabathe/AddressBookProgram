944bf74cd4fa5085b3bae5bdb0c16d08496b9164
pratikshabathe <pratikshabathe11@gmail.com>
2022-12-19 19:59:42 +0530

    Create New Brach

--- Main.java
+++ Main.java
@@ -4,67 +4,72 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-	
-	  static final Scanner SC = new Scanner(System.in);
+  static final Scanner SC = new Scanner(System.in);
 
-	  static HashMap<String, AddressBook> addressBookList = new HashMap<>();
+  static HashMap<String, AddressBook> addressBookList = new HashMap<>();
 
-	  public static void main(String[] args) {
-	    System.out.println("--Welcome to Address Book Program--");
+  public static void main(String[] args) {
+    System.out.println("--Welcome to Address Book Program--");
 
-	    String choice = "add";
+    String choice = "add";
 
-	    while (!choice.equals("quit")) {
-	      System.out.print("\nMain Menu \n1. create \n2. edit \n3. delete \n4. view \n5. quit \nEnter your choice: ");
-	      choice = SC.nextLine().trim().toLowerCase();
+    while (!choice.equals("quit")) {
+      System.out.print(
+        "\nMain Menu \n1. create \n2. edit \n3. delete \n4. view \n5. search person \n6. quit \nEnter your choice: ");
+      choice = SC.nextLine().trim().toLowerCase();
 
-	      switch (choice) {
-	        case "add":
-	        case "1":
-	          AddressBook a = new AddressBook();
-	          System.out.print("Enter a name for your addressbook ");
-	          String name = SC.nextLine();
-	          if (addressBookList.get(name) != null) {
-	            System.out.println("address book '" + name + "' already exists. enter a different name.");
-	            break;
-	          } else if (name.length() < 1) {
-	            System.out.println("address book name cannot be empty. try again.");
-	            break;
-	          }
-	          a.name = name;
-	          a.menu(a);
-	          addressBookList.put(name, a);
-	          System.out.println("address book '" + name + "' created.");
-	          break;
+      switch (choice) {
+        case "add":
+        case "1":
+          AddressBook a = new AddressBook();
+          System.out.print("Enter a name for your addressbook ");
+          String name = SC.nextLine();
+          if (addressBookList.get(name) != null) {
+            System.out.println("address book '" + name + "' already exists. enter a different name.");
+            break;
+          } else if (name.length() < 1) {
+            System.out.println("address book name cannot be empty. try again.");
+            break;
+          }
+          a.name = name;
+          a.menu(a);
+          addressBookList.put(name, a);
+          System.out.println("address book '" + name + "' created.");
+          break;
 
-	        case "edit":
-	        case "2":
-	          a = new AddressBook();
-	          addressBookList = a.viewEditDelete(addressBookList, "edit");
-	          break;
+        case "edit":
+        case "2":
+          a = new AddressBook();
+          addressBookList = a.viewEditDelete(addressBookList, "edit");
+          break;
 
-	        case "delete":
-	        case "3":
-	          a = new AddressBook();
-	          addressBookList = a.viewEditDelete(addressBookList, "delete");
-	          break;
+        case "delete":
+        case "3":
+          a = new AddressBook();
+          addressBookList = a.viewEditDelete(addressBookList, "delete");
+          break;
 
-	        case "view":
-	        case "4":
-	          a = new AddressBook();
-	          addressBookList = a.viewEditDelete(addressBookList, "view");
-	          break;
+        case "view":
+        case "4":
+          a = new AddressBook();
+          addressBookList = a.viewEditDelete(addressBookList, "view");
+          break;
 
-	        case "quit":
-	        case "5":
-	          choice = "quit";
-	          break;
+        case "search":
+        case "5":
+          a = new AddressBook();
+          a.search(addressBookList);
+          break;
 
-	        default:
-	          System.out.println("that didnt match any choice, try again");
-	          break;
+        case "quit":
+        case "6":
+          choice = "quit";
+          break;
 
-	      }
+        default:
+          System.out.println("that didnt match any choice, try again");
+          break;
+      	}
 	    }
 	  }
 }
